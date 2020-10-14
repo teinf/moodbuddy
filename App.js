f9bd1b5ca30561706728818059fc8711de42ab7d
Jakub Łotysz <jakublotysz@gmail.com>
2020-10-14 13:52:56 +0200

    Kalendarz - Działająca wersja testowa

--- App.js
+++ App.js
@@ -4,11 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 import Card from './app/components/Card';
 
 import ExampleScreen from './app/screens/ExampleScreen';
+import CalendarScreen from './app/screens/CalendarScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ExampleScreen/>
+      <CalendarScreen/>
     </View>
   );
 }
