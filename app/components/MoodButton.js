6f7b6173cb5e411d4acc1c56331f76e67c7dddaf
Nyzio <52327043+Nyzio229@users.noreply.github.com>
2020-10-18 17:57:07 +0200

    no nastroje

--- app/components/MoodButton.js
+++ app/components/MoodButton.js
@@ -6,11 +6,11 @@ import Card from "./Card";
 import Colors from "../constants/colors";
 import colors from "../constants/colors";
 
-export default function FlatButton({text, onPress}){
+export default function FlatButton({item, onPress, style}){
     return(
-        <TouchableOpacity onPress={onPress}>
-            <Card style={styles.button}>
-                <Text style={styles.buttonText}>{text}</Text>
+        <TouchableOpacity onPress={onPress} >
+            <Card style={[styles.button, style]}>
+                <Text style={styles.buttonText}>{item.text}</Text>
             </Card>
         </TouchableOpacity>
     )
@@ -22,14 +22,16 @@ const styles = StyleSheet.create({
         
         justifyContent: "center",
         backgroundColor: colors.lightAccent,
-        width: 120,
-        height: 50
+        width: 100,
+        height: 50,
     },
     buttonText: {
         color: '#000',
         fontWeight: 'bold',
         textTransform: 'uppercase',
-        fontSize: 16,
+        fontSize: 14,
         textAlign: 'center'
     }
-})
+
+});
+
---
+++ app/constants/emotions.js
@@ -0,0 +1,9 @@
+export default{
+    0: "Wesoły",
+    1: "Zadowolony",
+    2: "Pewny siebie",
+    3: "Stres",
+    4: "Nuda",
+    5: "Zabieganie",
+    
+}
