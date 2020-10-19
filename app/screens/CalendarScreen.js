0121f3aa993906b5421e08d26886619cbfa5ae3b
Jakub Łotysz <jakublotysz@gmail.com>
2020-10-19 18:26:27 +0200

    Usprawnienia nad CalendarScreen: - CalendarScreen poprawie pobiera Dane z "generateDates.js", - Możliwość zapisania więcej niż jednego Dnia, - Kolor Dnia w Kalendarzu jest zależny od Humoru

--- app/screens/CalendarScreen.js
+++ app/screens/CalendarScreen.js
@@ -5,6 +5,9 @@ import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
 import Colors from "../constants/colors";
 
 import {LocaleConfig} from 'react-native-calendars';
+import MoodColors from "../constants/moodColors";
+import MoodNames from "../constants/moodNames";
+import generateDates from "../utils/generateDates";
 
 LocaleConfig.locales['pl'] = {
   monthNames: ['Styczeń','Luty','Marzec','Kwiecień','Maj','Czerwiec','Lipiec','Sierpień','Wrzesień','Październik','Listopad','Grudzień'],
@@ -15,39 +18,70 @@ LocaleConfig.locales['pl'] = {
 };
 LocaleConfig.defaultLocale = 'pl';
 
-var dateList = [];
+
+// Do tej zmiennej powinny zostać pobrane Dane z Pliku (dane o zaznaczonych Dniach)
+var daty = {}
+
 
 function CalendarScreen(props) {
-    
 
-    const [myMarkedDates, setMarkedDates] = useState({
-        '2020-10-16': {selected: true, marked: true, selectedColor: 'blue'}})
-
+    const [myMarkedDates, setMarkedDates] = useState();
+    var baza = generateDates();
 
     function MarkDay(day)
     {
         // var key = day.dateString;
-        // var json = { };
-        // myMarkedDates[key] = {selected: true, marked: true, selectedColor: 'blue'}
-        // setMarkedDates((currentMarkedDates) => currentMarkedDates[key] = {selected: true, marked: true, selectedColor: 'blue'})
+        // var dayTimestamp = day.timestamp;
 
-        // var key2 = "2020-10-10";
-        // json[key2] = {selected: true, marked: true, selectedColor: 'blue'}
+        for(var timestamp in baza)
+        {
+            var nowaData = { };
+            var key = ConvertTimestampToKey(parseInt(timestamp));
+            //console.log(key);
+
+            var mood = baza[timestamp]["mood"];
+            var color = MoodColors[mood];
+
+
+            // if key exists = nie zamieniaj tej rzeczy, tylko dopisz
+            nowaData[key] = {selected: true, marked: true, selectedColor: color, timestamp: timestamp};
+            
+            daty = {...daty, ...nowaData};
+        }
+
+        setMarkedDates(daty);
     }
 
     return (
         <View>
             <CalendarList
+                // renderDay={(day, item) => {return (<View />);}}
                 onDayPress={(day) => {MarkDay(day)}}
+                onDayLongPress={(day) => {console.log('selected day', day)}}
                 firstDay={1}
                 horizontal={true}
                 pagingEnabled={true}
-                pastScrollRange={1000}
-                futureScrollRange={1000}
+                pastScrollRange={10}
+                futureScrollRange={10}
                 markedDates={myMarkedDates}
             />
         </View>
       );
+}
+
+function ConvertTimestampToKey(timestamp)
+{
+    var date = new Date(timestamp);
+
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    
+    if (day < 10) {day = "0" + String(day)};
+    if (month < 10) {month = "0" + String(month)};
+
+    var key = year + "-" + month + "-" + day;
+    return key;
 }
 
 const styles = StyleSheet.create({
