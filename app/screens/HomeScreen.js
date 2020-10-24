22dc4b346fae077f8473b0f293187f2769845dee
Kamil Kozioł <koziol.kamil@protonmail.com>
2020-10-24 19:37:54 +0200

    cojest

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,23 +1,37 @@
-import React, { useState } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
-import DateChanger from '../components/DateChanger';
-import MoodSlider from '../components/MoodSlider';
-import Colors from '../constants/colors';
+import React, { useState } from "react";
+import { Text, View, StyleSheet, Button } from "react-native";
+import DateChanger from "../components/DateChanger";
+import MoodSlider from "../components/MoodSlider";
+import Colors from "../constants/colors";
 
 import saveData from "../utils/saveData";
 import getData from "../utils/getData";
 import getAllData from "../utils/getAllData"
 
-    return (
-        <View style={styles.screen}>
-            <Text style={styles.welcomeText}>
-                Jak się masz?
-            </Text>
-            <MoodSlider onValueChange={(value) => null}/>
-            <DateChanger date={currentDate} mode="datetime" style={styles.dateChanger}/>
-            <Button title="Zapisz"/>
-        </View>
-    );
+function HomeScreen({ navigation }) {
+  const [currentDate, setCurrentDate] = useState(new Date(Date.now()));
+  const [mood, setMood] = useState(3);
+
+  async function onSaveButtonPress() {
+    await saveData(currentDate.getTime().toString(), {
+      mood: mood,
+      emotions: [1, 2, 3],
+    });
+  }
+
+  return (
+    <View style={styles.screen}>
+      <Text style={styles.welcomeText}>Jak się masz?</Text>
+      <MoodSlider onValueChange={(moodValue) => setMood(moodValue)} />
+      <DateChanger
+        date={currentDate}
+        mode="datetime"
+        style={styles.dateChanger}
+      />
+
+      <Button title="Zapisz" onPress={onSaveButtonPress} />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
