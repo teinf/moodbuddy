a0ec9cb5b8b3ea484c3d6e9548e7f598d456fda1
Nyzio <52327043+Nyzio229@users.noreply.github.com>
2020-10-25 18:08:46 +0100

    Alert
    Done

---
+++ app/constants/complimentsBad.js
@@ -0,0 +1,13 @@
+const ComplimentesBad = [
+    "Pomyśl o czymś miłym",
+    "Rozchmurz się",
+    "Nie poddawaj się",
+    "Złość piękności szkodzi",
+    "Nie złośc się już",
+    "Z każdej sytuacji da się wyjśc, pamiętaj aby myśleć pozytywnie",
+    "Nie pasuje do Ciebie zły humor",
+    "Zły nastruj nikomu nie służy",
+    "Idź naprzód",
+    "Nie przejmuj się",
+]
+export default ComplimentesBad;
---
+++ app/constants/complimentsGood.js
@@ -0,0 +1,9 @@
+const ComplimentesGood = [
+    "Tak trzymaj!",
+    "Nie zatrzymuj się",
+    "Tak trzymaj!",
+    "Taki nastrój Ci pasuje",
+    "Jest Ci do twarzy gdy masz dobry humor",
+    "Zdecydowanie lepiej mieć dobry humor, czyż nie?",
+]
+export default ComplimentesGood;
